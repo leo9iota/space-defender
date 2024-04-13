36e4a3951fc45705f27c8324f3f2d9a8386d5ffb
leo9iota <leo9iota@gmail.com>
2024-04-13 12:41:35 +0200

    Refactor canvas rendering code and add Player and Bullet classes

--- main.js
+++ main.js
@@ -1,44 +1,23 @@
-const canvasElement = document.querySelector('canvas');
-const canvas = canvasElement.getContext('2d');
+import Player from './Player.js';
+import Bullet from './Bullet.js';
 
+const canvasElement = document.querySelector('canvas');
+const canvasContext = canvasElement.getContext('2d');
+
+// Set the canvas width and height to match browser window
 canvasElement.width = window.innerWidth;
 canvasElement.height = window.innerHeight;
 
+// Constants for defining the center of the canvas
 const xCanvasCenter = canvasElement.width / 2;
 const yCanvasCenter = canvasElement.height / 2;
 
-class Player {
-  constructor(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
-
-  draw() {
-    canvas.beginPath();
-    canvas.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    canvas.fillStyle = this.color;
-    canvas.fill();
-  }
-}
-
-class Bullet {
-  constructor(x, y, radius, color, velocity) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-    this.velocity = velocity;
-  }
-
-  draw() {
-    canvas.beginPath();
-    canvas.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    canvas.fillStyle = this.color;
-    canvas.fill();
-  }
-}
-
-const player = new Player(xCanvasCenter, yCanvasCenter, 30, 'blue');
+const player = new Player(xCanvasCenter, yCanvasCenter, 30, 'blue', canvasContext);
 player.draw();
+
+const bullet = new Bullet();
+bullet.draw();
+
+addEventListener('click', () => {
+ 
+})
