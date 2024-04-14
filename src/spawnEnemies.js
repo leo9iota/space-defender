3249f8a59e5c4c7db548e7b54631ee9d529421dd
leo9iota <leo9iota@gmail.com>
2024-04-14 16:28:31 +0200

    Add enemy spawning on canvas

--- src/spawnEnemies.js
+++ src/spawnEnemies.js
@@ -1,6 +1,20 @@
-function spawnEnemies() {
+import Enemy from './classes/Enemy.js';
+
+function spawnEnemies(enemies, canvasContext) {
   setInterval(() => {
-    console.log('go');
+    const x = 100;
+    const y = 100;
+    const radius = 30;
+    const color = 'green';
+    const velocity = {
+      x: 1,
+      y: 1,
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity, canvasContext));
+
+    console.log(enemies);
   }, 1000);
 }
 
+export { spawnEnemies };
