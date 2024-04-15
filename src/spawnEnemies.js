615999fe2a958447bfd39cf50b3b65f25ecfe152
leo9iota <leo9iota@gmail.com>
2024-04-15 22:05:09 +0200

    Add random enemy spawn point

--- src/spawnEnemies.js
+++ src/spawnEnemies.js
@@ -1,14 +1,17 @@
 import Enemy from './classes/Enemy.js';
 
-function spawnEnemies(enemies, canvasContext) {
+function spawnEnemies(canvasContext, canvasElement, enemies) {
   setInterval(() => {
-    const x = 100;
-    const y = 100;
     const radius = 30;
+    const x = Math.random() < 0.5 ? 0 - radius : canvasElement.width + radius;
+    const y = Math.random() < 0.5 ? 0 - radius : canvasElement.height + radius;
     const color = 'green';
+
+    const angle = Math.atan2(canvasElement.height / 2 - y, canvasElement.width / 2 - x);
+
     const velocity = {
-      x: 1,
-      y: 1,
+      x: Math.cos(angle),
+      y: Math.sin(angle),
     };
 
     enemies.push(new Enemy(x, y, radius, color, velocity, canvasContext));
