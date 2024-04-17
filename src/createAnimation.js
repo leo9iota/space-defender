7e97a25956aa726692f1ebb7b5710f470f613ac3
leo9iota <leo9iota@gmail.com>
2024-04-17 20:49:17 +0200

    Add player collision detection

--- src/createAnimation.js
+++ src/createAnimation.js
@@ -1,9 +1,10 @@
+let animationId;
+
 function createAnimation(canvasContext, canvasElement, player, bullets, enemies) {
-  requestAnimationFrame(() =>
+  animationId = requestAnimationFrame(() =>
     createAnimation(canvasContext, canvasElement, player, bullets, enemies)
   );
 
-  // Clear bullet trail
   canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
   player.draw();
@@ -15,6 +16,13 @@ function createAnimation(canvasContext, canvasElement, player, bullets, enemies)
   enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
 
+    const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+    // End game
+    if (distance - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId);
+    }
+
     bullets.forEach((bullet, bulletIndex) => {
       // Get Euclidean distance between bullet and enemy
       const distance = Math.hypot(bullet.x - enemy.x, bullet.y - enemy.y);
@@ -23,7 +31,7 @@ function createAnimation(canvasContext, canvasElement, player, bullets, enemies)
       // If they overlap (distance between centers is less than sum of radii),
       // remove both from their respective arrays.
       if (distance - enemy.radius - bullet.radius < 1) {
-        // Fix flashing effect on collision
+        // Wait for next frame to start removing enemy from array to fix flash effect
         setTimeout(() => {
           enemies.splice(enemyIndex, 1);
           bullets.splice(bulletIndex, 1);
