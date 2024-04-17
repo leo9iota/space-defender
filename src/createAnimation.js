ed556afb0a32467798dad87a6ce89ea5addb887b
leo9iota <leo9iota@gmail.com>
2024-04-17 20:29:06 +0200

    Add collision detection

--- src/createAnimation.js
+++ src/createAnimation.js
@@ -1,6 +1,9 @@
 function createAnimation(canvasContext, canvasElement, player, bullets, enemies) {
-  requestAnimationFrame(() => createAnimation(canvasContext, canvasElement, player, bullets, enemies));
+  requestAnimationFrame(() =>
+    createAnimation(canvasContext, canvasElement, player, bullets, enemies)
+  );
 
+  // Clear bullet trail
   canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
   player.draw();
@@ -9,8 +12,21 @@ function createAnimation(canvasContext, canvasElement, player, bullets, enemies)
     bullet.update();
   });
 
-  enemies.forEach((enemy) => {
+  enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
+
+    bullets.forEach((bullet, bulletIndex) => {
+      // Get Euclidean distance between bullet and enemy
+      const distance = Math.hypot(bullet.x - enemy.x, bullet.y - enemy.y);
+
+      // Check for collision between bullet and enemy.
+      // If they overlap (distance between centers is less than sum of radii),
+      // remove both from their respective arrays.
+      if (distance - enemy.radius - bullet.radius < 1) {
+        enemies.splice(enemyIndex, 1);
+        bullets.splice(bulletIndex, 1);
+      }
+    });
   });
 }
 
