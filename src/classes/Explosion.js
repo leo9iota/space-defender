5aee5e29238f0674ba8dd76be22e1f36ba00d547
leo9iota <leo9iota@gmail.com>
2024-04-18 21:25:43 +0200

    Add fade animation to `Explosion` class

--- src/classes/Explosion.js
+++ src/classes/Explosion.js
@@ -5,20 +5,25 @@ class Explosion {
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
+    this.alpha = 1;
     this.canvasContext = canvasContext;
   }
 
   draw() {
+    this.canvasContext.save();
+    this.canvasContext.globalAlpha = this.alpha;
     this.canvasContext.beginPath();
     this.canvasContext.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     this.canvasContext.fillStyle = this.color;
     this.canvasContext.fill();
+    this.canvasContext.restore();
   }
 
   update() {
     this.draw();
     this.x = this.x + this.velocity.x;
     this.y = this.y + this.velocity.y;
+    this.alpha -= 0.01; 
   }
 }
 
