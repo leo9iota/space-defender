b6af6cc4ea8f8b609c69333e946004d96b9a5c45
leo9iota <leo9iota@gmail.com>
2024-04-18 16:05:30 +0200

    Add randomized enemy colors

--- src/spawnEnemies.js
+++ src/spawnEnemies.js
@@ -4,7 +4,7 @@ function spawnEnemies(canvasContext, canvasElement, enemies) {
   setInterval(() => {
     let x, y;
     const radius = Math.random() * (30 - 10) + 10;
-    const color = 'green';
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
     if (Math.random() < 0.5) {
       x = Math.random() < 0.5 ? 0 - radius : canvasElement.width + radius;
@@ -14,6 +14,7 @@ function spawnEnemies(canvasContext, canvasElement, enemies) {
       y = Math.random() < 0.5 ? 0 - radius : canvasElement.height + radius;
     }
 
+    // Calculate trajectory path of enemies
     const angle = Math.atan2(canvasElement.height / 2 - y, canvasElement.width / 2 - x);
 
     const velocity = {
