7a79572805e1f4738ca7718ccfb00648eb150c45
leo9iota <leo9iota@gmail.com>
2024-04-18 16:05:03 +0200

    Increase bullet velocity by specific factor

--- src/eventListener.js
+++ src/eventListener.js
@@ -6,12 +6,13 @@ function eventListener(canvasContext, xCanvasCenter, yCanvasCenter, bullets) {
     
     const bulletAngle = Math.atan2(event.clientY - yCanvasCenter, event.clientX - xCanvasCenter);
 
+    // Increase bullet velocity by specific factor (5 times)
     const bulletVelocity = {
-      x: Math.cos(bulletAngle),
-      y: Math.sin(bulletAngle),
+      x: Math.cos(bulletAngle) * 5,
+      y: Math.sin(bulletAngle) * 5,
     };
 
-    bullets.push(new Bullet(xCanvasCenter, yCanvasCenter, 5, 'red', bulletVelocity, canvasContext));
+    bullets.push(new Bullet(xCanvasCenter, yCanvasCenter, 5, 'white', bulletVelocity, canvasContext));
   });
 }
 
